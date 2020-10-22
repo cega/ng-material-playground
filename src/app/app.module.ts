97f7676b7519bfced71cca6ddd4c696d6d81f021
cega <cgalindez@gmail.com>
2020-10-22 11:03:10 -0300

    Add BrowsersAnimationModule to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { FormsModule } from "@angular/forms";
 
 import { HttpClientModule } from "@angular/common/http";
@@ -23,6 +25,7 @@ import { CategoryComponent, QuestionComponent, TagComponent } from "./components
     ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule,
