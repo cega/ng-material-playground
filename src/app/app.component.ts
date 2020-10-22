08e82488915c2fa38db506afa707291aa71aa540
cega <cgalindez@gmail.com>
2020-10-22 10:16:17 -0300

    Add material.module (barrel file). Changed title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  title = 'Angular + Material + Firebase Playground'
+  version = 'Angular ' + VERSION.major;
 }
